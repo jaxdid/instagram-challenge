398926bee7a832a51c9f54ffce8c21338b55cd71
Alain Lemaire <jaxdid@gmail.com>
2016-03-05 22:49:14 +0100

    Added user sign-out functionality
    - Added feature test and functionality for user sign-out.
    - Refactored existing feature test for specificity (and double-quotes!).
    - Added conditionality to account management links based on current
      session (whether a user is signed in or not).
    - Light refactoring of Devise default language ("Log in" -> "Sign in").

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -1,8 +1,37 @@
-feature 'Users' do
-  context 'that are not signed in'
-    scenario 'are able to sign in or sign up via the app proper' do
+feature "Users" do
+  context "that are not signed in" do
+    scenario "are able to sign in or sign up via the app proper" do
       visit root_path
-      expect(page).to have_content 'Sign in'
-      expect(page).to have_content 'Sign up'
+      click_link "Sign up"
+      fill_in "Email", with: "admin@example.com"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
+      click_button "Sign up"
+      expect(page).not_to have_link "Sign in"
+      expect(page).not_to have_link "Sign up"
+      expect(page).to have_content "Welcome! You have signed up successfully."
+      expect(page).to have_link "Sign out"
     end
+
+    # scenario "are able to sign in via Facebook" do
+    #   visit root_path
+    #   expect(page).to have_link "Sign in via Facebook"
+    # end
+  end
+    
+  context "that are signed in" do
+    scenario "are able to sign out" do
+      visit root_path
+      click_link "Sign up"
+      fill_in "Email", with: "admin@example.com"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
+      click_button "Sign up"
+      click_link "Sign out"
+      expect(page).not_to have_link "Sign out"
+      expect(page).to have_content "Signed out successfully."
+      expect(page).to have_link "Sign in"
+      expect(page).to have_link "Sign up"
+    end
+  end
 end
