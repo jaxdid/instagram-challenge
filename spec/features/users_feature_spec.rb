324dc9f52e775e83e0f45370bc64fc0856ccff14
Alain Lemaire <jaxdid@gmail.com>
2016-03-05 22:11:49 +0100

    Set up index view and user authentication
    - Added `Devise` gem to handle user authentication.
    - Updated development environment config with default URL options, as per Devise instructions.
    - Generated User model via Devise.
    - Copied in Devise user authentication views for customization.
    - Updated db schema via migration.
    - Set root route to `Application#index` action.
    - Added `#index` action to Application controller.
    - Refactored sign in/up feature test for context.

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -1,7 +1,7 @@
 feature 'Users' do
   context 'that are not signed in'
-    scenario 'are able to sign in or sign up' do
-      visit '/'
+    scenario 'are able to sign in or sign up via the app proper' do
+      visit root_path
       expect(page).to have_content 'Sign in'
       expect(page).to have_content 'Sign up'
     end
