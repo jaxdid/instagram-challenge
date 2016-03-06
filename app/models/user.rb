fbd1f4b8485d336a5473195c81d3799a83b15c7c
Alain Lemaire <jaxdid@gmail.com>
2016-03-06 16:57:44 +0100

    Added authentication via Facebook and Twitter
    - Updated OmniauthCallbacks controller and User model to support
      authentication via Facebook and Twitter.
    - Added usernames and images (profile pictures) to User objects, to pull
      in content from social media providers.
    - Updated Devise views to support usernames and password optionality
      (sign-in via Twitter).

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,20 +5,35 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, omniauth_providers: [:facebook, :twitter]
 
+  # attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  validates_presence_of :email
+  validates_uniqueness_of :email
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.username = auth.info.nickname
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      # user.image = auth.info.image # assuming the user model has an image
+      user.name = auth.info.name
+      user.image = auth.info.image
     end
   end
 
   def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"], without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
       end
+    else
+      super
     end
+  end
+
+  def password_required?
+    super && provider.blank?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306000109) do
+ActiveRecord::Schema.define(version: 20160306150139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20160306000109) do
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
+    t.string   "image"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
